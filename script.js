aae15e40739c98b3f698315846272baaf73644c7
ToastedThomas <blaketeahelser@gmail.com>
2023-05-30 00:54:03 -0500

    HTML cleanup
    Modal windows are now created dynamically with a javascript function.

--- script.js
+++ script.js
@@ -1,43 +1,136 @@
-const closeBtn = document.getElementsByClassName('close');
-const modalMain = document.getElementById('modal');
+const projectModals = [
+  {
+    title: "Pachinko Builder",
+    node: document.getElementById('pb'),
+    desc: "This was a project I submitted for the 'Made with Defold 2023' game jam. This jam lasted seven days in total, and the rules stated that submissions must be made with the Defold game engine. This was a challenging experience for me because I had never used Defold before that point, nor did I have any knowledge about Lua, which is what Defold used as its programming language.",
+    imgUrl: "images/captures/pbScreen1.png",
+    link: "https://toastedthomas.itch.io/pachinko-builder",
+    skills: ["Lua", "Time management", "Defold"]
+  },
+  {
+    title: "Better Exchange",
+    node: document.getElementById('be'),
+    desc: "Better Exchange is a web application that uses the official Runescape Wiki API to grab information about any item listed on the Grand Exchange. This program provides up to date prices on items, daily volumes, alchemy values, and much more. The application also calculates the potential profits that could be made from various activities in game such as cooking or fletching.",
+    imgUrl: "images/captures/beScreen1.png",
+    link: "https://toastedthomas.github.io/BetterExchange",
+    skills: ["APIs", "JavaScript", "HTML"]
+  },
+  {
+    title: "Crop Clicker",
+    node: document.getElementById('cc'),
+    desc: "This was my first project I began work on while I taught myself programming. Crop clicker is a web browser game that is heavily inspired from Cookie Clicker by Orteil. I used this project as a way to build on my HTML, CSS, and JavaScript knowledge, as well as to learn other skills such as launching, updating, and bug fixing a live website. I also made all the art and sound for this project myself.",
+    imgUrl: "images/captures/ccScreen1.png",
+    link: "https://www.cropclicker.com",
+    skills: ["JavaScript", "CSS", "Git", "Adobe Photoshop"]
+  },
+  {
+    title: "Falling Sand",
+    node: document.getElementById('fs'),
+    desc: "Falling Sand is a simulator of exactly what it says, falling sand. This project was meant to refine my skills as a JavaScript developer, making me think logically and creatively.",
+    imgUrl: "images/captures/fsScreen1.png",
+    link: "https://toastedthomas.github.io/Falling-Sand/",
+    skills: ["JavaScript", "HTML"]
+  },
+  {
+    title: "Dice Roller",
+    node: document.getElementById('dr'),
+    desc: "I originally started this project to mess around with arrays and random number generation. As time went on I added multiple dice types and dice statistics. My goal with this project is to add more customization options and features while keeping this tool lightweight and fast. During experimental testing, I was able to roll 250,000 dice at once!",
+    imgUrl: "images/captures/drScreen1.png",
+    link: "https://toastedthomas.github.io/Dice-Roller-Js/",
+    skills: ["JavaScript", "CSS", "HTML"]
+  },
+  {
+    title: "PokeInfo",
+    node: document.getElementById('pi'),
+    desc: "This project was my introduction to working with APIs and working with other peoples documentation. My goal was to take in data from the PokeAPI database and display the information in an easy to understand format. Reading and fully understanding someone else's code is a very important skill to have, and working on this project helped refine that skill for me.",
+    imgUrl: "images/captures/piScreen1.png",
+    link: "https://toastedthomas.github.io/PokeInfo/",
+    skills: ["APIs", "JavaScript", "CSS"]
+  },
+  {
+    title: "Sample Survey Form",
+    node: document.getElementById('ss'),
+    desc: "This is a basic survey that showcases some of my skills I learned while taking the 'Responsive Web Design' course at freeCodeCamp.",
+    imgUrl: "images/captures/ssScreen1.png",
+    link: "https://toastedthomas.github.io/Sample-Survey/",
+    skills: ["CSS", "WCAG", "HTML"]
+  }
+]
 
-const projectTiles = [document.getElementById('be'), document.getElementById('cc'), document.getElementById('fs'), document.getElementById('dr'), document.getElementById('pi'),  document.getElementById('r2s'), document.getElementById('yt'), document.getElementById('ss'), document.getElementById('donut')]
-const myWorkTiles = [document.getElementById('be'), document.getElementById('cc'), document.getElementById('fs'), document.getElementById('dr'), document.getElementById('pi'),  document.getElementById('ss')];
-const modalContent = [document.getElementById('beModal'), document.getElementById('ccModal'), document.getElementById('fsModal'), document.getElementById('drModal'), document.getElementById('piModal'),  document.getElementById('artModal1'), document.getElementById('artModal2'), document.getElementById('ssModal'), document.getElementById('artModal3')]
-
-// array.from takes the Dom collection and turns it into an array to use
-Array.from(closeBtn).forEach((element) => {
-  element.onclick = function() {
-    modalMain.style.animation = "disappear 0.25s";
-    setTimeout(function() {
-      modalMain.style.display = "none";
-      modalContent.forEach(element => {element.style.display = "none";})
-      modalMain.style.animation = "none";
-    }, 240);
-  };
-});
-
-for(let e of myWorkTiles) {
-  e.addEventListener("mouseenter", function(e) {
+projectModals.forEach((element, index) => {
+  element.node.addEventListener("click", function() { createModal(element) })
+  element.node.addEventListener("mouseenter", function(e) {
     e.target.childNodes[1].src = "gifs/" + e.target.id + ".gif";
     e.target.childNodes[1].style.objectFit = "cover";
   });
-  e.addEventListener("mouseleave", function(e) {
+  element.node.addEventListener("mouseleave", function(e) {
     e.target.childNodes[1].src = "images/captures/" + e.target.id + "Tile.png";
     e.target.childNodes[1].style.objectFit = "fill";
   });
+})
+
+function createModal(element) {
+  //creating modal elements
+  const modalOverlay = document.createElement('div');
+  modalOverlay.id = "modal";
+  document.getElementById('body').appendChild(modalOverlay);
+
+  const modalBox = document.createElement('div');
+  modalBox.classList.add("center");
+  modalBox.classList.add("modalContent");
+  modalOverlay.appendChild(modalBox);
+
+  const closeButton = document.createElement('p');
+  closeButton.addEventListener("click", function() { deleteModal(); });
+  closeButton.classList.add("close");
+  closeButton.innerHTML = "x";
+  modalBox.appendChild(closeButton);
+
+  const flexBox = document.createElement('div');
+  flexBox.classList.add("modalFlex");
+  modalBox.appendChild(flexBox);
+
+  const section1 = document.createElement('div');
+  flexBox.appendChild(section1);
+
+  const title = document.createElement('h1');
+  title.innerHTML = element.title;
+  section1.appendChild(title);
+
+  const desc = document.createElement('p');
+  desc.classList.add("modalP1")
+  desc.innerHTML = element.desc;
+  section1.appendChild(desc);
+  
+  const skillBox = document.createElement('div');
+  skillBox.classList.add('skillBox');
+  element.skills.forEach(skillTxt => {
+    const skill = document.createElement('p');
+    skill.classList.add('skillBubble');
+    skill.innerHTML = skillTxt
+    skillBox.appendChild(skill);
+  });
+  section1.appendChild(skillBox)
+
+  const img = document.createElement('img');
+  img.src = element.imgUrl;
+  flexBox.appendChild(img);
+
+  const linkDesc = document.createElement('h2');
+  linkDesc.innerHTML = "Check out " + element.title + " ";
+  modalBox.appendChild(linkDesc);
+
+  const link = document.createElement('a');
+  link.innerHTML = "here!"
+  link.target = "_blank";
+  link.href = element.link;
+  linkDesc.appendChild(link);
+
 }
 
-projectTiles.forEach((element, index) => {
-  element.onclick = function() {
-    modalMain.style.animation = "disappear 0.25s reverse";
-    setTimeout(function() {
-      modalMain.style.animation = "none";
-    }, 240);
-    modalMain.style.display = "block";
-    modalContent[index].style.display = "block";
-  }
-});
+function deleteModal() {
+  document.getElementById("modal").remove();
+}
 
 //code for grid effect
 
